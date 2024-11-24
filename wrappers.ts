1e7f4377566bfe4a2ac04b374d5a9387c9182559
Panth <panth977@Panths-MacBook-Pro.local>
2024-11-24 16:48:03 +0530

    export default input/output types

--- wrappers.ts
+++ wrappers.ts
@@ -6,4 +6,4 @@ export * from "./wrappers/clone.ts";
 export * from "./wrappers/debug.ts";
 export * from "./wrappers/memo.ts";
 export * from "./wrappers/parse.ts";
-export * from "./wrappers/helper.ts";
+export * from "./wrappers/_helper.ts";
